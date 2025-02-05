307c0a70196994f6674e27eb060bb636c1ee7dbf
oderahub <okere_chidera@yahoo.com>
2025-02-05 02:14:09 +0100

    feat:app setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
 const PORT = process.env.PORT || 8000
 
 app.use(express.json())
+app.use(cors())
 
 app.disable('x-powered-by')
 
